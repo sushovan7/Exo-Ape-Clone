f0d5b3ff0f6409248f472940f53a0fad656e539a
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-17 22:53:23 +0545

    .

---
+++ src/components/Work.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+
+function Work() {
+ 
+  return (
+    <div className="w-full relative">
+      <div className="max-w-screen-2xl py-20 px-5 sm:px-10 sm:py-10 sm:ml-20 sm:relative">
+        <div className="featured sm:absolute flex sm:right-[30%] sm:top-[8%] gap-3">
+          <svg
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-4"
+            data-v-669b4a84=""
+          >
+            <path
+              d="M7.41908 4.56679L6.13722 0L4.85418 4.92566L0 6L4.85418 7.25435L6.13722 12L7.3276 7.25435L12 6L7.41908 4.56679Z"
+              fill="currentColor"
+              data-v-669b4a84=""
+            ></path>
+          </svg>
+          <h3 className="capitalize">featured projects</h3>
+        </div>
+        <div className="py-7 sm:py-14">
+          <h1 className="text-6xl sm:text-[15rem]">Work</h1>
+        </div>
+        <p className="text-xl sm:w-[30%] sm:absolute  sm:right-[11%] sm:top-[9%] leading-2 sm:text-2xl ">
+          Highlights of cases that we passionately built with forward-thinking
+          clients and friends over the years
+        </p>
+        <div className="elems sm:flex-wrap sm:flex mt-7">
+          <div className="elem w-full  sm:w-[50%]">
+            <div className="video relative w-full  overflow-hidden h-[60vw]">
+              <img
+                className="hidden sm:block w-full h-full object-cover"
+                src="https://a.storyblok.com/f/133769/2409x3000/b5ccf4619b/studio-d-hero.jpg/m/1300x1619/filters:format(jpeg):quality(70)"
+                alt=""
+              />
+              <video
+                autoPlay
+                muted
+                loop
+                className="block sm:hidden w-full scale-[1.3] h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="https://player.vimeo.com/progressive_redirect/playback/914798702/rendition/720p/file.mp4?loc=external&log_user=0&signature=b76cb0994de6beca7f6ba29e7eb52db1750d34600352dab2566c6ca16fcd817f"
+              ></video>
+            </div>
+          </div>
+          <div className="w-full mt-4 leading-2">
+            <h3 className="capitalize font-semibold text-lg">studio d</h3>
+            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+              Urban and landscape design
+            </h5>
+          </div>
+          <div className="elem w-full  sm:w-[35%] ">
+            <div className="video relative w-full overflow-hidden h-[35vw]">
+              <img
+                className="hidden sm:block w-full h-full object-cover"
+                src="https://a.storyblok.com/f/133769/2500x3113/78edf86efd/rino-pelle-hero.jpg/m/1300x1619/filters:format(jpeg):quality(70)"
+                alt=""
+              />
+              <video
+                autoPlay
+                muted
+                loop
+                className="block sm:hidden w-full scale-[1.3] h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="https://player.vimeo.com/progressive_redirect/playback/914798484/rendition/720p/file.mp4?loc=external&log_user=0&signature=666d8099e0f1361eb1766ec0592b63a9bdf2b303cdb12ddfa5789d20ce8f523b"
+              ></video>
+            </div>
+          </div>
+          <div className="w-full mt-4 leading-2">
+            <h3 className="capitalize font-semibold text-lg">rino and pelle</h3>
+            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+            effortless chic lifestyle
+            </h5>
+          </div>
+          <div className="elem w-full  sm:w-[50%]">
+            <div className="video relative w-full overflow-hidden h-[105vw]">
+              <img
+                className="hidden sm:block w-full h-full object-cover"
+                src="https://a.storyblok.com/f/133769/1606x2000/09a30057bd/aebele-interiors-hero.jpg/m/1300x1619/filters:format(jpeg):quality(70)"
+                alt=""
+              />
+              <video
+                autoPlay
+                muted
+                loop
+                className="block sm:hidden w-full scale-[1.3] h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src=   "https://player.vimeo.com/progressive_redirect/playback/914796979/rendition/540p/file.mp4?loc=external&log_user=0&signature=57ac30ff6fc3b29802f7414617e808395306f0b0fa5c9c6e3ef483a532f49c15"
+              ></video>
+            </div>
+          </div>
+          <div className="w-full mt-4 leading-2">
+            <h3 className="capitalize font-semibold text-lg">aebele interiors</h3>
+            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+            luxurios design experience
+            </h5>
+          </div>
+          <div className="elem w-full  sm:w-[50%]">
+            <div className="video relative w-full overflow-hidden h-[105vw]">
+              <img
+                className="hidden sm:block w-full h-full object-cover"
+                src="https://a.storyblok.com/f/133769/2880x3588/5c50befb8e/pixel-flakes-hero.jpg/m/1300x1620/filters:format(jpeg):quality(70)"
+                alt=""
+              />
+              <video
+                autoPlay
+                muted
+                loop
+                className="block sm:hidden w-full scale-[1.3] h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="https://player.vimeo.com/progressive_redirect/playback/914798271/rendition/720p/file.mp4?loc=external&log_user=0&signature=2af493277d050009920c26416a8d62a9a8c316353242c609ee157fcf1e300076"
+              ></video>
+            </div>
+          </div>
+          <div className="w-full mt-4 leading-2">
+            <h3 className="capitalize font-semibold text-lg">pixelflakes</h3>
+            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+            architectural marketing agency
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Work;
