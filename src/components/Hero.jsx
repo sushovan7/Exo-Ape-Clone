c7dbe0de77c295ad3759820e5bdd943f0269d0d3
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-18 23:00:37 +0545

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="w-screen relative h-screen bg-black text-white ">
-        <div className="w-48 overflow-hidden aspect-video  absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="w-screen relative h-screen bg-black  text-white ">
+        <div className="w-48  sm:w-[30vw] overflow-hidden aspect-video  absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <video className="w-full scale-[1.2] h-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" autoPlay loop muted src="https://obys.agency/wp-content/uploads/2022/11/Obys-Showreel-2022.mp4"></video>
         </div>
-      <div className="overlay absolute w-full h-full flex flex-col py-20 justify-between ">
+      <div className="overlay sm:px-32 absolute w-full h-full flex flex-col py-20 justify-between ">
         <div className="flex w-full  items-center justify-center gap-3">
           <svg
             viewBox="0 0 12 12"
@@ -23,12 +23,12 @@ function Hero() {
           </svg>
           <h3 className="capitalize text-sm">work in motion</h3>
         </div>
-        <h1 className="text-4xl flex items-center w-full justify-center gap-32">
+        <h1 className="text-4xl sm:text-9xl flex items-center sm:justify-between w-full justify-center gap-32">
           <div>Play</div>
           <div>Reel</div>
         </h1>
-        <p className="px-10 text-xs text-center">
-          Our work is best experience in motion . Don't forget to put on your
+        <p className="px-10 sm:text-sm text-xs text-center">
+          Our work is best experience in motion . Don't <br /> forget to put on your
           headphone
         </p>
       </div>
