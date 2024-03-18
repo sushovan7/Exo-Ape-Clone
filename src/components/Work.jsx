6852b6e73160349a062bd24dc28ba98e9470e688
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-18 21:52:41 +0545

    .

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
 function Work() {
- 
   return (
     <div className="w-full relative">
       <div className="max-w-screen-2xl py-20 px-5 sm:px-10 sm:py-10 sm:ml-20 sm:relative">
-        <div className="featured sm:absolute flex sm:right-[30%] sm:top-[8%] gap-3">
+        <div className="featured sm:absolute flex sm:right-[30%] sm:top-[17%] gap-3">
           <svg
             viewBox="0 0 12 12"
             fill="none"
@@ -24,13 +23,13 @@ function Work() {
         <div className="py-7 sm:py-14">
           <h1 className="text-6xl sm:text-[15rem]">Work</h1>
         </div>
-        <p className="text-xl sm:w-[30%] sm:absolute  sm:right-[11%] sm:top-[9%] leading-2 sm:text-2xl ">
+        <p className="text-xl sm:w-[30%] sm:absolute  sm:right-[11%] sm:top-[19%] leading-2 sm:text-2xl ">
           Highlights of cases that we passionately built with forward-thinking
           clients and friends over the years
         </p>
         <div className="elems sm:flex-wrap sm:flex mt-7">
           <div className="elem w-full  sm:w-[50%]">
-            <div className="video relative w-full  overflow-hidden h-[104vw] sm:h-[60vw]">
+            <div className="video relative w-full  overflow-hidden h-[104vw] sm:h-[50vw]">
               <img
                 className="hidden sm:block w-full h-full object-cover"
                 src="https://a.storyblok.com/f/133769/2409x3000/b5ccf4619b/studio-d-hero.jpg/m/1300x1619/filters:format(jpeg):quality(70)"
@@ -44,14 +43,15 @@ function Work() {
                 src="https://player.vimeo.com/progressive_redirect/playback/914798702/rendition/720p/file.mp4?loc=external&log_user=0&signature=b76cb0994de6beca7f6ba29e7eb52db1750d34600352dab2566c6ca16fcd817f"
               ></video>
             </div>
+            <div className="w-full mt-4 leading-2">
+              <h3 className="capitalize font-semibold text-lg">studio d</h3>
+              <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+                Urban and landscape design
+              </h5>
+            </div>
           </div>
-          <div className="w-full mt-4 leading-2">
-            <h3 className="capitalize font-semibold text-lg">studio d</h3>
-            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
-              Urban and landscape design
-            </h5>
-          </div>
-          <div className="elem  w-full sm:w-[35%] ">
+
+          <div className="elem  w-full sm:-mt-[40%] sm:w-[30%] sm:ml-[60%]">
             <div className="video relative w-full overflow-hidden sm:h-[35vw] h-[104vw]">
               <img
                 className="hidden sm:block w-full h-full object-cover"
@@ -66,15 +66,18 @@ function Work() {
                 src="https://player.vimeo.com/progressive_redirect/playback/914798484/rendition/720p/file.mp4?loc=external&log_user=0&signature=666d8099e0f1361eb1766ec0592b63a9bdf2b303cdb12ddfa5789d20ce8f523b"
               ></video>
             </div>
+            <div className="w-full mt-4 leading-2">
+              <h3 className="capitalize font-semibold text-lg">
+                rino and pelle
+              </h3>
+              <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+                effortless chic lifestyle
+              </h5>
+            </div>
           </div>
-          <div className="w-full mt-4 leading-2">
-            <h3 className="capitalize font-semibold text-lg">rino and pelle</h3>
-            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
-            effortless chic lifestyle
-            </h5>
-          </div>
-          <div className="elem w-full  sm:w-[50%]">
-            <div className="video relative w-full overflow-hidden h-[105vw]">
+
+          <div className="elem w-full  sm:w-[35%] sm:ml-[40%] sm:mt-[12%]">
+            <div className="video relative w-full overflow-hidden sm:h-[50vw] h-[105vw]">
               <img
                 className="hidden sm:block w-full h-full object-cover"
                 src="https://a.storyblok.com/f/133769/1606x2000/09a30057bd/aebele-interiors-hero.jpg/m/1300x1619/filters:format(jpeg):quality(70)"
@@ -85,18 +88,20 @@ function Work() {
                 muted
                 loop
                 className="block sm:hidden w-full scale-[1.3] h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                src=   "https://player.vimeo.com/progressive_redirect/playback/914796979/rendition/540p/file.mp4?loc=external&log_user=0&signature=57ac30ff6fc3b29802f7414617e808395306f0b0fa5c9c6e3ef483a532f49c15"
+                src="https://player.vimeo.com/progressive_redirect/playback/914796979/rendition/540p/file.mp4?loc=external&log_user=0&signature=57ac30ff6fc3b29802f7414617e808395306f0b0fa5c9c6e3ef483a532f49c15"
               ></video>
             </div>
+            <div className="w-full mt-4 leading-2">
+              <h3 className="capitalize font-semibold text-lg">
+                aebele interiors
+              </h3>
+              <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+                luxurios design experience
+              </h5>
+            </div>
           </div>
-          <div className="w-full mt-4 leading-2">
-            <h3 className="capitalize font-semibold text-lg">aebele interiors</h3>
-            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
-            luxurios design experience
-            </h5>
-          </div>
-          <div className="elem w-full  sm:w-[50%]">
-            <div className="video relative w-full overflow-hidden h-[105vw]">
+          <div className="elem w-full  sm:w-[25%] sm:-ml-[70%] sm:mt-[55%]">
+            <div className="video relative w-full overflow-hidden sm:h-[25vw] h-[105vw]">
               <img
                 className="hidden sm:block w-full h-full object-cover"
                 src="https://a.storyblok.com/f/133769/2880x3588/5c50befb8e/pixel-flakes-hero.jpg/m/1300x1620/filters:format(jpeg):quality(70)"
@@ -110,12 +115,14 @@ function Work() {
                 src="https://player.vimeo.com/progressive_redirect/playback/914798271/rendition/720p/file.mp4?loc=external&log_user=0&signature=2af493277d050009920c26416a8d62a9a8c316353242c609ee157fcf1e300076"
               ></video>
             </div>
-          </div>
-          <div className="w-full mt-4 leading-2">
-            <h3 className="capitalize font-semibold text-lg">pixelflakes</h3>
-            <h5 className="capitalize text-zinc-500 mb-7 text-lg">
-            architectural marketing agency
-            </h5>
+            <div className="w-full mt-4 leading-2">
+              <h3 className="capitalize font-semibold text-lg">
+                aebele interiors
+              </h3>
+              <h5 className="capitalize text-zinc-500 mb-7 text-lg">
+                luxurios design experience
+              </h5>
+            </div>
           </div>
         </div>
       </div>
