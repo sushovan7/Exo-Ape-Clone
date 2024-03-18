67bb00503cbac2f45aafe3e0eadd972604e7d545
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-19 02:02:48 +0545

    .

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 
 function Work() {
   return (
@@ -10,7 +10,7 @@ function Work() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className="w-4"
-            data-v-669b4a84=""
+            data-v-669b4a84="" 
           >
             <path
               d="M7.41908 4.56679L6.13722 0L4.85418 4.92566L0 6L4.85418 7.25435L6.13722 12L7.3276 7.25435L12 6L7.41908 4.56679Z"
