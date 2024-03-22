011869cd1a0a3a52e564011abe1580444b656cba
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-22 17:37:36 +0545

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-100% h-screen overflow-hidden bg-black px-7 py-16 flex flex-col items-start justify-center gap-4 text-[#E0CCBB]'>
-        <div className='flex items-center gap-2 mb-7'>
+    <div className='w-100% h-screen overflow-hidden bg-black px-7 py-16 sm:p-20 flex flex-col items-start justify-center gap-4 text-[#E0CCBB]'>
+        <div className='flex items-center gap-40 overflow-hidden rounded-full mb-7'>
             <h1 className='w-40% text-6xl '>Our Story</h1>
-            <video src=""></video>
+            <video autoPlay loop muted className='w-50%' src="https://player.vimeo.com/progressive_redirect/playback/561971096/rendition/360p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=c5755b14fbf7a235976f8df6233d269ab41af83d34c937c70fdd3f6e7e79c2da"></video>
         </div>
         <p className='text-lg w-full mb-4'> The story behind Exo Ape is one of exploration,creativity and curiosity.</p>
         <h5 className='border-b-[1px] border-zinc-700 font-semibold'>Our Story</h5>
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 function Images() {
   return (
-    <div className="w-full overflow-hidden h-[70vh] bg-white flex items-center justify-center">
-      <div className="w-[40%] h-1/2 relative">
-        <div className="absolute w-[8vh] h-[12vh] -right-[25%] top-6">
+    <div className="w-full overflow-hidden h-[70vh] sm:h-[100vh] bg-white flex items-center justify-center">
+      <div className="w-[40%] sm:h-[70%] h-1/2 sm:w-[30%] relative">
+        <div className="absolute sm:w-[20vh] sm:h-[25vh] w-[8vh] h-[12vh] -right-[25%] top-6">
             <img className="w-full h-full object-cover" src="https://a.storyblok.com/f/133769/348x494/21becfd449/home-news-3.jpg/m/550x781/filters:format(jpeg):quality(70)" alt="" />
         </div>
-        <div className="absolute -left-[53%] top-[35%] aspect-video w-[15vh] h-[9vh]">
+        <div className="absolute sm:h-[25vh] sm:aspect-video sm:scale-[1.4] sm:w-[25vh] -left-[53%] sm:top-[30%] top-[35%] aspect-video w-[15vh] h-[9vh]">
             <video className="w-full h-full scale-[1.2]" autoPlay loop muted src="https://a.storyblok.com/f/133769/x/9c433c7aac/home-news-diesel-be-a-follower.mp4"></video>
         </div>
-        <div className="absolute -left-[75%] -bottom-[15%] w-[18vh] h-[9vh]">
+        <div className="absolute -left-[75%] -bottom-[15%] sm:w-[30vh] sm:h-[25vh] w-[18vh] sm:-bottom-[30%] sm:-left-[50%] h-[9vh]">
         <img className="w-full h-full object-cover" src="https://a.storyblok.com/f/133769/758x508/8a1ff60d00/home-news-4.jpg/m/550x369/filters:format(jpeg):quality(70)" alt="" />
         </div>
-        <div className=" absolute aspect-[1.5/1] w-[20vh] h-[15vh] -right-[75%] -bottom-[29%]">
+        <div className="absolute aspect-[1.5/1] w-[20vh] sm:w-[30vh] sm:h-[32vh] h-[15vh] -right-[75%] -bottom-[29%] sm:-right-[50%]">
             <video className="" autoPlay loop muted src="https://a.storyblok.com/f/133769/x/88b4bf7989/news-rino-pelle.mp4"></video>
         </div>
         <img
