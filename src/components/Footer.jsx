c7d74956e377e67c3e87ffc96f312ea39475a173
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-22 17:42:19 +0545

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='w-100% h-screen overflow-hidden bg-black px-7 py-16 sm:p-20 flex flex-col items-start justify-center gap-4 text-[#E0CCBB]'>
         <div className='flex items-center gap-40 overflow-hidden rounded-md mb-7'>
-            <h1 className='w-40% text-6xl '>Our Story</h1>
+            <h1 className='w-40% sm:text-9xl text-6xl '>Our Story</h1>
             <video autoPlay loop muted className='w-50%' src="https://cdn.dribbble.com/users/4210486/screenshots/18120551/media/9adb3522e6ae76a0126db802da4be6d5.mp4"></video>
         </div>
         <p className='text-lg w-full mb-4'> The story behind Exo Ape is one of exploration,creativity and curiosity.</p>
