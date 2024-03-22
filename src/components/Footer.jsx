a46c01fb50b2e2ac445a7f7b958d38c42511b975
sushovan7 <bhattaraisushovan999@gmail.com>
2024-03-22 17:44:41 +0545

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
   return (
     <div className='w-100% h-screen overflow-hidden bg-black px-7 py-16 sm:p-20 flex flex-col items-start justify-center gap-4 text-[#E0CCBB]'>
-        <div className='flex items-center gap-40 overflow-hidden rounded-md mb-7'>
+        <div className='flex items-center gap-4 overflow-hidden rounded-md mb-7'>
             <h1 className='w-40% sm:text-9xl text-6xl '>Our Story</h1>
-            <video autoPlay loop muted className='w-50%' src="https://cdn.dribbble.com/users/4210486/screenshots/18120551/media/9adb3522e6ae76a0126db802da4be6d5.mp4"></video>
+            <video autoPlay loop muted className='w-50% sm:w-[50%]' src="https://cdn.dribbble.com/users/4210486/screenshots/18120551/media/9adb3522e6ae76a0126db802da4be6d5.mp4"></video>
         </div>
         <p className='text-lg w-full mb-4'> The story behind Exo Ape is one of exploration,creativity and curiosity.</p>
         <h5 className='border-b-[1px] border-zinc-700 font-semibold'>Our Story</h5>
